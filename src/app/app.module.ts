1ee072e43775f842ca8fd341711b8a673a4d491b
rlagunov <roman.lagunov@anaplan.com>
2021-05-18 22:30:07 -0700

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,9 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,10 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
